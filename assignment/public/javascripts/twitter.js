c07443ebeb7ec2307f73fe6dba84a162d8639daf
shannon <shannon.herondale@gmail.com>
2019-10-22 18:29:57 +1000

    Display tweets with sentiment analysis

--- assignment/public/javascripts/twitter.js
+++ assignment/public/javascripts/twitter.js
@@ -1,16 +1,24 @@
-var Twit = require('twit');
+let Twit = require('twit');
+let Sentiment = require('sentiment');
 const CONFIG = require("../../config.json");
 
-var T = new Twit({
+let T = new Twit({
     consumer_key: CONFIG.twitter_api_key,
     consumer_secret: CONFIG.twitter_api_secret,
     access_token: CONFIG.twitter_access_token,
     access_token_secret: CONFIG.twiiter_access_secret
-  })
+});
 
-async function getTweets(params) {
-    T.get('search/tweets', { q: 'cloud', count: 2 }, function(err, data, response) {
-        console.log(data.statuses);
+let sentiment = new Sentiment();
+
+async function getTweets() {
+    T.get('search/tweets', { q: 'cat', count: 2 }, function(err, data, response) {
+        let tweets = data.statuses;
+        for (let i = 0; i < tweets.length; i++) {
+            let result  = JSON.stringify(sentiment.analyze(tweets[i].text).score);
+            console.log('result: ' + result + '. Text: ' + tweets[i].text);
+            //console.log(JSON.stringify(result));
+        }
     });
 }
 
