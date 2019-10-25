97ff45c2bde97572cb8c930074d859643558bf3f
MFredX <47466168+MFredX@users.noreply.github.com>
2019-10-25 17:24:22 +1000

    Got some chart to be displayed
    Not sure if this is the way to approach the code

--- assignment/routes/results.js
+++ assignment/routes/results.js
@@ -6,11 +6,22 @@ var twitter = require('../public/javascripts/twitter');
 let Twit = require('twit');
 let Sentiment = require('sentiment');
 const CONFIG = require("../config.json");
+const chart = require('chart.js')
+
+var responses_x_questions = require('../public/javascripts/barcharts.json');
+var clone = require('clone');
 
 /* POST results - listening. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   var hashtags = req.query["hashtags"];
   hashtags = hashtags.split(",");
+
+
+  var chartOptions = clone(responses_x_questions);
+  var categories = ["newCat1", "newCat2", "newCat3", "newCat4", "newCat5"];
+  chartOptions.xAxis[0].data = categories;
+  chartOptions.series[0].data = [10, 20, 30, 40, 50];
+
 
   //Send error page if array is empty
   if (hashtags == "") {
@@ -39,22 +50,39 @@ router.get("/", function(req, res, next) {
 
   let sentiment = new Sentiment();
 
- 
-  T.get('search/tweets', { q: hashtags, count: 5, language: 'en' }, function(err, data, response) {
+
+  T.get('search/tweets', { q: hashtags, count: 10, language: 'en' }, function (err, data, response) {
     let tweets = data.statuses;
     let responses = [];
     for (let i = 0; i < tweets.length; i++) {
       tweet = tweets[i].text;
-      let result  = JSON.stringify(sentiment.analyze(tweet).score);
+      let result = JSON.stringify(sentiment.analyze(tweet).score);
       //console.log('result: ' + result + '. Text: ' + tweet);
       //console.log(JSON.stringify(result));
       responses.push([tweet, result]);
     }
-    console.log(responses);
-    res.status(200).render("results", { hashtags: hashtags, tweetData: responses});
+    // console.log(responses);
+    counts = scoreTracker(responses);
+    console.log(counts);
+    // console.log(counts[10]);
+    // console.log("This is the score of the 10th Tweet: " + responses[10][1]);
+    res.status(200).render("results", { hashtags: hashtags, tweetData: responses, data: JSON.stringify(chartOptions) });
   });
 
+  //This function calculates the count of tweets for each score and returns an object with 
+  //the corresponding information
+  function scoreTracker(responses) {
+    var counts = {};
+    for (let i = 0; i < responses.length; i++) {
+      //Accessing the score of each tweet in responses array
+      const element = responses[i][1];
+      counts[element] = counts[element] ? counts[element] + 1 : 1;
+      //Priniting each element for verification
+      // console.log(element);
 
+    }
+    return counts;
+  }
   //TODO: Get sentiment analysis for each set of tweets
 
   //TODO: Convert analysis to charts
